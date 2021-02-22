2c96a45fbf430c2ed09944d6d8d4f59ec958cda1
AlopexMM <mariomori00@gmail.com>
2021-02-22 16:33:38 -0300

    Setup fixed

--- setup.py
+++ setup.py
@@ -1,10 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="Fiscal-tools",
-    version="0.1.0",
-    packages=find_packages(include=["afip", "afip.*"]),
-    install_requires = [],
-    setup_requires=["pytest-runner", "flake8"],
-    tests_require=["pytest"]
-)
+from setuptools import setup
+setup()
